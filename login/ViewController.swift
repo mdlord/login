9f21029a1409cf56abf1232aaf05436f937c0fda
mdlord <mayankdaswani123@yahoo.com>
2017-01-04 16:12:39 -0500

    added camera and gallery

--- login/ViewController.swift
+++ login/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var camera: UIButton!
+    @IBOutlet weak var gallery: UIButton!
+    @IBOutlet weak var imagedisp: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,11 +28,34 @@ class ViewController: UIViewController {
     }
     let pref = UserDefaults.standard
     
+    @IBAction func camerabtn(_ sender: Any) {
+        
+        let pick = UIImagePickerController()
+        pick.delegate = self
+        pick.sourceType = .camera
+        
+        present(pick, animated:true, completion:nil)
+    }
+    
+    @IBAction func gallerybtn(_ sender: Any) {
+        
+        let pick = UIImagePickerController()
+        pick.delegate = self
+        pick.sourceType = .photoLibrary
+        
+        present(pick, animated:true, completion:nil)
+    }
+    
     @IBAction func logoutbutton(_ sender: Any) {
         pref.set(false,forKey:"isUserLoggedIn")
         pref.synchronize()
         self.performSegue(withIdentifier: "loginView", sender:self)
         
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imagedisp.image = info[UIImagePickerControllerOriginalImage] as? UIImage;
+        dismiss(animated: true, completion:nil)
     }
     override func viewDidAppear(_ animated: Bool) {
         let userloggedin = pref.bool(forKey: "isUserLoggedIn")
